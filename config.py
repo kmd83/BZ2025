0292a6211fe99f4fd17f203d9b6ed497db98da73
kmd83 <kmd@ukr.net>
2025-09-19 19:19:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = 8481174858:AAFl49MPR7eMgpgnW1V2R6dfU1aLm2fBvGI
 GOALS = {"loss": "Похудение", "maintain": "Поддержка", "gain": "Набор"}
 ACTIVITY = {1:1.2, 2:1.375, 3:1.55, 4:1.725, 5:1.9}
